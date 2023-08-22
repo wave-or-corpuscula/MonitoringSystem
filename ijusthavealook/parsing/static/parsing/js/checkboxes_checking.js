338415149a3b14891046323e0a25e8f95401d477
andrey <ninzyago@gmail.com>
2023-08-22 15:39:03 +0300

    correct check all display + code reducing

--- ijusthavealook/parsing/static/parsing/js/checkboxes_checking.js
+++ ijusthavealook/parsing/static/parsing/js/checkboxes_checking.js
@@ -14,35 +14,49 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 
-    function createNewIdBlock(id, text) {
+    document.getElementById('check-all-checkboxes').addEventListener('click', function () {
+        checkboxes.forEach(checkbox => {
+            if (!checkbox.checked) {
+                checkbox.checked = true;
+                const checkboxText = checkbox.getAttribute('name');
+                createNewIdBlock(checkbox.id, checkboxText);
+            }
+        });
+    });
+
+    document.getElementById('clear-checkboxes').addEventListener('click', function () {
+        checkboxes.forEach(checkbox => {
+            if (checkbox.checked) {
+                checkbox.checked = false;
+                removeIdBlock(checkbox.id);
+            }
+        });
+    });
+
+    function createNewIdBlock(id, checkboxText) {
         const newElement = document.createElement('div');
         newElement.className = 'selected-id-block';
         newElement.setAttribute('data-id', id);
-        // newElement.textContent = fetchCompanyData(id, newElement);
-        fetchCompanyData(id, newElement);
+        fetchCompanyData(id, newElement, checkboxText);
         selectedIdsBlock.appendChild(newElement);
     }
 
-    function fetchCompanyData(observedId, newElement) {
+    function fetchCompanyData(observedId, newElement, checkboxText) {
         fetch(`/get_goods_data/?store_id=${observedId}`)
                .then(response => response.json())
                .then(data => {
-                   const prices = data.prices;
-                   displayStorePrices(prices, newElement);
+                    newElement.innerHTML = generatePricesHtml(data.prices, checkboxText);
                })
                .catch(error => console.error('Error fetching store prices:', error));
     }
 
-    function displayStorePrices(prices, newElement) {
-        if (prices.length > 0) {
-            let pricesHtml = 'Цены для выбранного магазина:<br>';
-            prices.forEach(price => {
-                pricesHtml += `Товар ID: ${price.good_id}, Цена: ${price.price}<br>`;
-            });
-            newElement.innerHTML = pricesHtml;
-        } else {
-            newElement.textContent = 'Нет доступных цен для выбранного магазина';
-        }
+    function generatePricesHtml(prices, checkboxText) {
+        let pricesHtml = '<p>' + checkboxText + ':</p><ul>';
+        prices.forEach(price => {
+            pricesHtml += `<li>Товар ID: ${price.good_id}, Цена: ${price.price}</li>`;
+        });
+        pricesHtml += '</ul>';
+        return pricesHtml;
     }
 
     function removeIdBlock(id) {
--- ijusthavealook/parsing/templatetags/parsing_tags.py
+++ ijusthavealook/parsing/templatetags/parsing_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from parsing.models import Observed
+from parsing.models import Observed, Goods
 
 register = template.Library()
 
@@ -7,3 +7,8 @@ register = template.Library()
 @register.simple_tag(name='getobs')
 def get_observed():
     return Observed.objects.all()
+
+
+@register.simple_tag(name='getgoods')
+def get_goods():
+    return Goods.objects.all()
